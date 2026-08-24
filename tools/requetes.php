871157587e6778b115bc3e1e4c5e8a38a2d5b74e
Cowboy de la programmation <aleksandr.lkn@eduge.ch>
2026-08-24 15:58:54 +0200

    Requetes a la base de donnes et son dockerfile

---
+++ tools/requetes.php
@@ -0,0 +1,140 @@
+<?php
+
+require_once "pdo.php";
+
+function ajouterUnUser(string $nom, string $mdp): bool
+{
+    $pdo = connexionBdd();
+
+    $sql = "INSERT INTO utilisateur (nom, mdp)
+            VALUES (:nom, :mdp)";
+
+    $stmt = $pdo->prepare($sql);
+
+    $param = [
+        "nom" => $nom,
+        "mdp" => password_hash($mdp, PASSWORD_DEFAULT)
+    ];
+
+    return $stmt->execute($param);
+}
+
+function UserExist(string $nom)
+{
+    $pdo = connexionBdd();
+
+    $sql = "SELECT nom
+            FROM utilisateur
+            WHERE nom = ?";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$nom]);
+
+    return $stmt->fetch();
+}
+
+function verifierDonneesUser(string $nom, string $mdp): bool
+{
+    $pdo = connexionBdd();
+
+    $sql = "SELECT mdp
+            FROM utilisateur
+            WHERE nom = ?";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$nom]);
+
+    $user = $stmt->fetch();
+
+    if (!$user) {
+        return false;
+    }
+
+    return password_verify($mdp, $user["mdp"]);
+}
+
+function updateToken(string $nom)
+{
+    $pdo = connexionBdd();
+
+    $sql = "UPDATE utilisateur
+            SET token = ?
+            WHERE nom = ?";
+
+    $stmt = $pdo->prepare($sql);
+
+    $token = uniqid('', true);
+
+    return $stmt->execute([$token, $nom]) ? $token : false;
+}
+
+function verifierUnToken(string $token)
+{
+    $pdo = connexionBdd();
+
+    $sql = "SELECT token
+            FROM utilisateur
+            WHERE token = ?";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$token]);
+
+    return $stmt->fetch();
+}
+
+function supprimerUnToken(string $nom): bool
+{
+    $pdo = connexionBdd();
+
+    $sql = "UPDATE utilisateur
+            SET token = NULL
+            WHERE nom = ?";
+
+    $stmt = $pdo->prepare($sql);
+
+    return $stmt->execute([$nom]);
+}
+
+function recupererUnUserAvecToken(string $token)
+{
+    $pdo = connexionBdd();
+
+    $sql = "SELECT *
+            FROM utilisateur
+            WHERE token = ?";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$token]);
+
+    return $stmt->fetch();
+}
+
+function recupererEvenementsPrevus(): array
+{
+    $pdo = connexionBdd();
+
+    $sql = "SELECT idEvenement, titre, dateDebut, lieu
+            FROM evenement
+            WHERE dateFin >= NOW()
+            ORDER BY dateDebut ASC";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    return $stmt->fetchAll();
+}
+
+function recupererEvenementsArchives(): array
+{
+    $pdo = connexionBdd();
+
+    $sql = "SELECT *
+            FROM evenement
+            WHERE dateFin < NOW()
+            ORDER BY dateDebut DESC";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    return $stmt->fetchAll();
+}
